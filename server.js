a9747efc2eb6b0f9adf2074e37f352f5e46b3900
Pozvonok <workmailskachkov@gmail.com>
2025-10-20 23:23:53 +0300

    Fix: Listen on 0.0.0.0 for Render deployment

--- server.js
+++ server.js
@@ -280,7 +280,9 @@ function sendRoomUserList(clientId, roomId) {
 }
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Сервер запущен на http://localhost:${PORT}`);
+const HOST = '0.0.0.0';
+
+server.listen(PORT, HOST, () => {
+    console.log(`Сервер запущен на http://${HOST}:${PORT}`);
 });
 
